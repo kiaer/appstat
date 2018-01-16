ee8bd4117099149fd5243d1133d0c065e186edd2
kiaer <s123812@student.dtu.dk>
2018-01-16 11:12:02 +0100

    Init results

--- Case2/case_campy.R
+++ Case2/case_campy.R
@@ -8,23 +8,26 @@ library(lattice)
 
 summary(camp)
 #plot(camp, panel=panel.smooth)
-
 boxplot(pos/total ~ week, camp)
 
 plot(camp$week, camp$aveTemp)
 plot(camp$week, camp$pos/camp$total)
 
-lm1 <- lm((pos / total) ~ (I(aveTemp^2) + maxTemp + relHum + sunHours + precip + week)^2, data=camp)
-par(mfrow=c(2,2))
-plot (lm1)
+lm1 <- lm((pos / total) ~ (aveTemp + maxTemp + relHum + sunHours + precip )^2, data=camp, subset = -496)
+par(mfrow=c(2,1))
+plot(lm1, which=1)
+plot(lm1, which=5)
+
 residualPlots(lm1)
 summary(lm1)
-boxcox(lm1)
+boxcox(lm1, lambda = seq(0, 1, length.out = 20), data=camp)
 
-lm2 <- step(lm1)
+lm2 <- stepP(lm1)$object
 summary(lm2)
 anova(lm2)
 drop1(lm2, test="F")
+library(xtable)
+xtable(residualPlots(lm1))
 
 stepP <- function(object, level=0.05, verbose=FALSE){
   if (!("lm" %in% class(object))){
@@ -72,6 +75,7 @@ a3 <- stepP(a1)$object
 drop1(a3, test="F")
 anova(a3)
 summary(a3)
+plot(a3)
 
 b1 <- lm((pos / total)^0.7 ~ (aveTemp + maxTemp + relHum   + precip  + sunHours + I(aveTemp^2) + I(sunHours^2) + I(precip^2))^2, data=camp, subset = -496)
 par(mfrow=c(2,2))
@@ -91,6 +95,23 @@ lec.fun<-function(data, reference, others=names(data)[names(data)!=reference], r
   }
   return(pdata)
 }
+
+pred.frame <- function(reference, data, others=names(data)[ !(names(data)%in%names(reference)) ]){
+  if (class(reference) == "list"){  ## Need to run expand.grid( <list>)
+    pdata <- expand.grid(reference)
+  } else {
+    pdata <- reference
+  }
+  ref.model <- names(reference)
+  if(length(names(reference))>1)
+    ref.model <- paste(ref.model, sep="+")
+  for(i in others){
+    lmtmp<-lm(as.formula(paste(i,"~",ref.model)),data)
+    pdata[[i]]<-predict(lmtmp,newdata=pdata)
+  }
+  return(pdata)
+}
+
 par(mfrow=c(1,1))
 plot((pos / total) ~ aveTemp,data=camp)
 
@@ -102,15 +123,23 @@ pred.campya<- predict(a3, int="p",newdata=campy)
 matlines(campy$aveTemp,pred.campy^(10/7),lty=c(1,2,2),col=3,lwd=2)
 matlines(campy$aveTemp,pred.campya^(10/7),lty=c(1,2,2),col=2,lwd=2)
 
-# x1p <- 0:30
-# x2p <- 0:30
-campy <- lec.fun(camp,reference="aveTemp",others=c("maxTemp", "relHum", "precip", "sunHours"), ref.values=10:20)
-pred.campy <- expand.grid(aveTemp=campy$aveTemp, relHum =campy$relHum, maxTemp = campy$maxTemp, sunHours=campy$sunHours, precip = campy$precip)
-# ## Then predicting each grid point:
-pred.campy$fit <- predict(a3, int = "p", newdata=pred.data)
-pred.campy$fit
-#wireframe(fit ~ aveTemp * precip, pred.data, scales = list(arrows = FALSE), drape=TRUE)
-#levelplot(fit^(10/7) ~ maxTemp * relHum, pred.data)
+p.sunHours <- seq(0,90,by = 3)
+p.aveTemp <- seq(0, 30)
+par(mfrow=c(1,1))
+
+## Creating prediction data.frame and then predicting
+pred.data <- pred.frame(reference = list(sunHours=p.sunHours, aveTemp=p.aveTemp), data = camp, others = c("maxTemp", "relHum", "precip"))
+pred <- predict(a3, newdata = pred.data, interval = "predict")
+
+## Wrapping the predictions as a matrix 
+z <- matrix(pred[,"fit"]^(10/7), nrow=length(p.sunHours))
+
+## First an image:
+image(p.sunHours, p.aveTemp, z, xlab = "sunHours", ylab = "Precipitation")
+## Adding a contour:
+contour(p.sunHours, p.aveTemp, z, add=TRUE, labcex = 1.5)
+points(aveTemp ~ sunHours, data= camp, cex=0.5) # To show the observations
+
 
 # a2 <- step(a1)
 # a3 <- stepP(a2)$object
